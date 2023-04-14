cb4ae561b88f128f4f558b63c063276681223770
bora-sen <bahattinborasen@gmail.com>
2023-04-14 21:56:43 +0300

    +configured packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-green": "#77aa5a",
+      },
+      fontFamily: {
+        display: ["Bebas Neue", "cursive"],
+      },
+    },
   },
   plugins: [],
 }
