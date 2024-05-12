29eef87780de1a632d442a45235b430541a441ec
bora-sen <bahattinborasen@gmail.com>
2024-05-12 23:18:00 +0300

    refactored main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
 import React from "react"
-import ThreeDBackground from "./Sections/ThreeDBackground"
 import * as Section from "./Sections"
 
 function App() {
   return (
-    <ThreeDBackground>
+    <Section.ThreeDBackground>
       <Section.Header />
       <Section.Hero />
       <Section.Services />
       <Section.Works />
       <Section.Contact />
       <Section.Footer />
-    </ThreeDBackground>
+    </Section.ThreeDBackground>
   )
 }
 
